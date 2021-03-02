284e116c42ff8c204993e19d4fa9d82b910f37cc
IfrimAndrei <andrei_ifrim@yahoo.com>
2021-03-02 13:46:20 +0200

    Compulsory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
 import Interfaces.Visitable;
 import Locations.Church;
+import Locations.Hotel;
 import Locations.Museum;
+import Locations.Restaurant;
 
 import java.time.LocalTime;
 
 
 public class Main {
     public static void main(String arggs[]) {
+        Hotel v1 =new Hotel();
+        Museum v2 = new Museum();
+        Museum v3 = new Museum();
+        Church v4 = new Church();
+        Church v5 = new Church();
+        Restaurant v6 = new Restaurant();
 
         Museum m = new Museum();
         m.setOpeningTime( LocalTime.of(9, 30)); //9:30
